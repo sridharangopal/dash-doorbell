641cb20a11597b462f298d25c366fccd1a7fc0cf
sridharangopal <sridharangopal@gmail.com>
2017-01-08 13:03:15 -0500

    added additional config

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Dial Liquid Hand Soap Dash MAC - ac:63:be:66:90:d3
 var dash_button = require('node-dash-button');
-var dash = dash_button("ac:63:be:66:90:d3", null, 5000, "all"); //address from step above
+var config = require('config');
+
+console.log("Press Ctrl + C to stop script ...");
+var dash = dash_button(config.get('dash.backdoor-mac'), null, 5000, "all"); //address from step above
+
 dash.on("detected", function (){
   console.log("dash button pressed");
 });
